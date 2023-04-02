661268fe3df948b8d630fc3bed987b9af0d1c849
RyanBeGell <Ryanbegell@outlook.com>
2023-04-01 21:26:31 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module awesomeProject
+
+go 1.18
+
+require github.com/gizak/termui/v3 v3.1.0
+
+require (
+	github.com/gen2brain/beeep v0.0.0-20230307103607-6e717729cb4f // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gen2brain/beeep"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
+
+const (
+	workMinutes  = 1
+	breakMinutes = 1
+)
+
+func main() {
+	if err := ui.Init(); err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	workDuration := time.Duration(workMinutes) * time.Minute
+	breakDuration := time.Duration(breakMinutes) * time.Minute
+	rounds := 4
+
+	// Create widgets
+	timer := widgets.NewParagraph()
+	timer.Title = "Pomodoro Timer \U0001F345"
+	timer.Text = formatTime(workDuration)
+	timer.TextStyle.Fg = ui.ColorGreen
+	timer.SetRect(0, 0, 50, 3)
+
+	status := widgets.NewParagraph()
+	status.Title = "Status"
+	status.Text = "Working"
+	status.TextStyle.Fg = ui.ColorYellow
+	status.SetRect(0, 4, 50, 5)
+
+	progress := widgets.NewGauge()
+	progress.Title = "Progress"
+	progress.Percent = 0
+	progress.BarColor = ui.ColorBlue
+	progress.SetRect(0, 6, 50, 7)
+
+	ui.Render(timer, status, progress)
+
+	for round := 1; round <= rounds; round++ {
+		runPomodoro(timer, status, progress, workDuration, "Working")
+		showNotification("Pomodoro Timer", "Time for a break!")
+		playSound()
+		runPomodoro(timer, status, progress, breakDuration, "Break")
+		showNotification("Pomodoro Timer", "Time to get back to work!")
+		playSound()
+	}
+
+	// Show completed message
+	status.Text = "Done"
+	ui.Render(timer, status, progress)
+	time.Sleep(3 * time.Second)
+}
+
+func runPomodoro(timer *widgets.Paragraph, status *widgets.Paragraph, progress *widgets.Gauge, duration time.Duration, statusText string) {
+	startTime := time.Now()
+	endTime := startTime.Add(duration)
+	ticker := time.NewTicker(time.Second)
+
+	for range ticker.C {
+		remaining := endTime.Sub(time.Now())
+		if remaining <= 0 {
+			break
+		}
+		timer.Text = formatTime(remaining)
+		progress.Percent = int((duration - remaining) * 100 / duration)
+		status.Text = statusText
+		ui.Render(timer, status, progress)
+	}
+
+	// Show completed message
+	timer.Text = formatTime(0)
+	status.Text = fmt.Sprintf("%s - Completed", statusText)
+	progress.Percent = 100
+	ui.Render(timer, status, progress)
+
+	// Wait for a second to show the completed message
+	time.Sleep(1 * time.Second)
+}
+
+func playSound() {
+	for i := 0; i < 3; i++ {
+		err := beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(500 * time.Millisecond) // Wait for 500 milliseconds between each beep
+	}
+}
+
+func showNotification(title, message string) {
+	iconPath := "./tomato-icon.png"
+	err := beeep.Notify(title, message, iconPath)
+	if err != nil {
+		return
+	}
+}
+
+func formatTime(duration time.Duration) string {
+	minutes := int(duration.Minutes())
+	seconds := int(duration.Seconds()) % 60
+	return fmt.Sprintf("%02d:%02d", minutes, seconds)
+}
